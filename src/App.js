13cc36aab857214c1fd0194e5133e0238017a071
MaryB16 <buda.maria89@gmail.com>
2018-07-20 17:07:30 +0300

    Deleted google places API
    added  2 test markers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import React, {Component} from 'react';
 import './App.css';
 import MapStyles from './MapStyles';
 
+const results =  [
+  {title:"locationOne", position:{ lat: 44.4314812, lng: 26.0983232 }},
+  {title:"locationTwo", position:{ lat: 44.431877, lng: 26.101609 }}
+]
+
 class App extends Component {
   componentDidMount() {
     let map = new google.maps.Map(document.getElementById('map'), {
@@ -11,31 +16,28 @@ class App extends Component {
         lat: 44.4318595,
         lng: 26.0991834
       },
-      zoom: 14,
+      zoom: 16,
       styles: MapStyles
     });
 
-    let service = new window.google.maps.places.PlacesService(map);
-    let markers =[];
+    let markers = [];
     let largeInfowindow = new google.maps.InfoWindow();
 
-    let placeMarkers = (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        results.forEach(function(result){
-          let marker = new google.maps.Marker({
-            position: result.geometry.location,
-            title: result.name,
-            map: map,
-            id:result.id,
-            animation: google.maps.Animation.DROP
-          })
-          markers.push(marker)
-          marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-          })
+    results.forEach(function(result) {
+      let marker = new google.maps.Marker({
+         position: result.position,
+         title: result.title,
+         map: map,
+         animation: google.maps.Animation.DROP
+       })
 
-          function populateInfoWindow(marker, infowindow) {
-        if (infowindow.marker != marker) {
+      markers.push(marker)
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+      })
+
+      function populateInfoWindow(marker, infowindow) {
+        if (infowindow.marker !== marker) {
           infowindow.marker = marker;
           infowindow.setContent('<div>' + marker.title + '</div>');
           infowindow.open(map, marker);
@@ -44,20 +46,8 @@ class App extends Component {
           });
         }
       }
-
-          console.log(marker)
-        })
-      }
-    }
-
-    service.nearbySearch({
-      location: {
-        lat: 44.4318595,
-        lng: 26.0991834
-      },
-      radius: 5000,
-      type: ['museum']
-    }, placeMarkers);
+      console.log(markers)
+    })
   }
 
   render() {
