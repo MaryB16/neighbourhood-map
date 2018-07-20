9de6000341a9918a079a890a17395ef4e04b7a0f
MaryB16 <buda.maria89@gmail.com>
2018-07-20 14:13:55 +0300

    Removed  react-google-maps library
    Replaced it with standard google maps API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React, { Component } from 'react';
+/*global google*/
+
+import React, {Component} from 'react';
 import './App.css';
-import MapComponent from './Map';
+import MapStyles from './MapStyles';
 
 class App extends Component {
+  componentDidMount() {
+    let map = new google.maps.Map(document.getElementById('map'), {
+      center: {
+        lat: 44.4318595,
+        lng: 26.0991834
+      },
+      zoom: 14,
+      styles: MapStyles
+    });
+
+    let service = new window.google.maps.places.PlacesService(map);
+
+    service.nearbySearch({
+      location: {
+        lat: 44.4318595,
+        lng: 26.0991834
+      },
+      radius: 4000,
+      type: ['museum']
+    }, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          new google.maps.Marker({
+            position: results[i].geometry.location,
+            title: results[i].name,
+            map: map,
+            animation: google.maps.Animation.DROP
+          })
+        }
+      }
+    });
+
+  }
+
   render() {
-    return (
-      <MapComponent
-        isMarkerShown={false}
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBB9mgpOXLSD2ds0Yl3okmuQaUcJkMNPuI&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100vh` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    );
+    return <div id="map"></div>;
   }
 }
 
--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,30 @@
+/* global google */
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import MapStyles from './MapStyles';
+
+const locations =  [
+  {title:"locationOne", position:{ lat: 44.4314812, lng: 26.0983232 }},
+  {title:"locationTwo", position:{ lat: 44.431877, lng: 26.101609 }}
+]
+
+fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=44.4318595,26.0991834&radius=1500&type=museum&key=AIzaSyBB9mgpOXLSD2ds0Yl3okmuQaUcJkMNPuI')
+.then(response => response.json())
+.then(myJson => console.log(myJson));
+
 
 const MapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={16}
     defaultCenter={{ lat: 44.4318595, lng: 26.0991834 }}
-    defaultOptions={{ styles: MapStyles }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    defaultOptions={{ styles: MapStyles }}>
+    {locations.map(location =>
+      <Marker
+        position={location.position}
+        title={location.title}
+        animation={google.maps.Animation.BOUNCE}
+      />
+    )}
   </GoogleMap>
 ))
 
